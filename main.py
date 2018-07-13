b9f773d08554e1a9d3d16536bef3cf65eeb85932
Chris Schneider <chrisschneider@Chriss-MacBook-Pro-3.local>
2018-07-14 00:27:42 +0200

    New architecture

--- main.py
+++ main.py
@@ -2,18 +2,8 @@ from __future__ import print_function
 from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
-
-def get_env_from_params(params):
-  if params.get('gender') is None: abort(400, "gender missing")
-  if params.get('age') is None: abort(400, "age missing")
-  if params.get('music_on') is None: abort(400, "music_on missing")
-  if params.get('has_sunglasses') is None: abort(400, "has_sunglasses missing")
-  return {
-    'gender': params.get('gender', type=int),
-    'age': params.get('gender', type=int),
-    'music_on': params.get('gender', type=int),
-    'has_sunglasses': params.get('has_sunglasses', type=int)
-  }
+step = 0
+should_reward_last_action = False
 
 @app.route('/')
 def hello():
@@ -21,14 +11,39 @@ def hello():
 
 @app.route('/next_action')
 def next_action():
-  env = get_env_from_params(request.args)
+  if (step >= 4):
+    abort(400, "End of simulation, please call /reset")
+  if params.get('gender') is None: abort(400, "gender missing")
+  if params.get('age') is None: abort(400, "age missing")
+  if params.get('music_on') is None: abort(400, "music_on missing")
+  if params.get('has_sunglasses') is None: abort(400, "has_sunglasses missing")
+  env = {
+    'gender': params.get('gender', type=int),
+    'age': params.get('gender', type=int),
+    'music_on': params.get('gender', type=int),
+    'has_sunglasses': params.get('has_sunglasses', type=int),
+    'step': step
+  }
+  action = 'MUSIC_A' # TODO: Get action
+  should_reward_last_action = True
+  step = += 1
   return jsonify({
-    'action': 'MUSIC_A',
+    'action': action,
     'env': env
   })
 
+@app.route('/observe')
+def reward():
+  if params.get('happiness') is None: abort(400, "happiness missing")
+  happiness = params.get('happiness', type=float)
+  if not should_reward_last_action:
+    # TODO: Reward
+    should_reward_last_action = true
+  return "OK"
+
 @app.route('/reset')
 def reset():
+  step = 0
   return "OK"
 
 if __name__ == "__main__":
