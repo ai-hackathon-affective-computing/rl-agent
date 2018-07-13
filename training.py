59906ad8e7b80d6a797daeb47086b8cbd8b3bb6c
Vowinckel <konrad.vowinckel@siemens.com>
2018-07-13 12:53:37 +0200

    Added actions to trainer and removed key bug in random_features_generator

--- training.py
+++ training.py
@@ -1,7 +1,7 @@
 from johannes_agent import JohannesAgent
 from environment import Environment
 
-from random import randint
+import random
 
 feature_def = {
     'gender': {'min': 0, 'max': 1},
@@ -11,14 +11,25 @@ feature_def = {
 }
 
 action_def = {
-    
+    0: 'music_A',
+    1: 'music_B',
+    2: 'route_A',
+    3: 'route_B'
 }
 
 
-def random_features_generator(seed=None):
+def random_features_generator(force_dict, seed=None):
+    force_dict = {} if force_dict is None else force_dict
     env = {}
+
+    if seed:
+        random.seed(seed)
+
     for key in feature_def.keys():
-        env['key'] = randint(feature_def[key]['min'], feature_def[key]['max'])
+        if key in force_dict.keys():
+            env['key'] = force_dict[key]
+        else:
+            env[key] = random.randint(feature_def[key]['min'], feature_def[key]['max'])
     return env
 
 
