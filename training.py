01bfbd6ab8e25afbd1d9d59a679b7f1689259d1c
Vowinckel <konrad.vowinckel@siemens.com>
2018-07-13 14:28:33 +0200

    Updated Simulation

--- training.py
+++ training.py
@@ -1,13 +1,14 @@
 from johannes_agent import JohannesAgent
-from environment import Environment
+from reward_simulator import simulate_reward
 
 import random
 
 feature_def = {
     'gender': {'min': 0, 'max': 1},
     'age': {'min': 18, 'max': 99},
-    'music_on': {'min': 0, 'max': 1},
     'has_sunglasses': {'min': 0, 'max': 1},
+    'music_on': {'min': 0, 'max': 1},
+    'step': {'min': 0, 'max': 1},               # This can be exchanged with time later
 }
 
 action_def = {
@@ -33,19 +34,39 @@ def random_features_generator(force_dict, seed=None):
     return env
 
 
+def update_features(features, step, is_music):
+    features['step'] = step
+    features['music_on'] = int(is_music)
+    return features
+
+
 if __name__ == '__main__':
 
     n_repetitions = 100
+    n_steps = 5             # 0, 1 (15min), 2 (30min), 3 (45min), 4 (END)
 
-    agent = JohannesAgent(n_features=4, n_actions=4)
-    environment = Environment()
-
-    last_action = 0
-    last_feature = None
-    reward = 0
+    agent = JohannesAgent(n_features=len(feature_def), n_actions=4)
 
     for repetition in range(n_repetitions):
-        features = random_features_generator()
-        action = agent.act(features=features, reward=reward)
 
-        reward = environment.simulate(features, action)
+        ### RESET ###
+        last_features = None
+        reward = 0
+        is_music = False
+        features = random_features_generator({'music_on': int(is_music)})
+
+        ### STEP ###
+        for step in range(n_steps):
+            action = agent.act(features=features.values())
+            last_features = features
+            features = update_features(features, step, is_music)
+            reward = simulate_reward(features, action)
+
+            agent.remember((last_features.values(), action, reward, features.values(), (step == n_steps-1)))
+
+            if 'music' in action_def[action]:
+                is_music = True
+
+        agent.learn()
+
+    agent.save()
