1e5adcba18269c98210a906ff5006e90e03e7a38
Johannes Handloser <Johannes.Handloser@bmw.de>
2018-07-13 13:15:33 +0200

    logic for rl-agent implemented (need to be tested)

--- johannes_agent.py
+++ johannes_agent.py
@@ -1,20 +1,103 @@
+from keras.models import *
+from keras.layers.core import Dense, Activation
+from random import sample as rsample
+import numpy as np
+import random as rn
+from copy import deepcopy
+import datetime
+
+
 class JohannesAgent(object):
-    def __init__(
-            self,
-            n_features=10,
-            n_actions=4,
-    ):
+    def __init__(self, n_features=10, n_actions=4, load_network=False, file=None):
+        self.experience_buffer = []
+        self.n_actions = n_actions
+        self.hyperparameters = self.read_in_configuration_file()[0]["hyperparameters"]
+        self.epsilon = self.hyperparameters["EPSILON"]
         self.n_features = n_features
         self.n_actions = n_actions
+        if load_network:
+            self.value_model = self.build_network(n_features, n_actions)
+            self.target_model = self.build_network(n_features, n_actions)
+        else:
+            self.value_model = self.load(file)
+            self.target_model = self.load(file)
+        self.counter = 0
 
-    def act(self, features, reward, done):
-        raise NotImplementedError
+    def act(self, features):
+        if np.random.rand() <= self.epsilon:
+            return rn.randint(0, self.n_actions - 1)
+        else:
+            act_values = self.target_model.predict(features)
+            return np.argmax(act_values[0])
 
-    def learn(self, list_obs_reward_tuples):
-        raise NotImplementedError
+    def learn(self):
+        if self.counter % 10 == 0:
+            self.target_model.set_weights(self.value_model.get_weights())
+            batch = self.create_batch()
+            if batch is None:
+                return
+            for state, action, reward, next_state, done in batch:
+                target = self.target_model.predict(state)
+                if done:
+                    target[0][action] = reward
+                else:
+                    a = self.value_model.predict(next_state)[0]
+                    t = self.target_model.predict(next_state)[0]
+                    target[0][action] = reward + self.hyperparameters["GAMMA"] * t[np.argmax(a)]
+                self.value_model.fit(state, target, epochs=1, verbose=0)
+                if self.epsilon > self.hyperparameters["EPSILON_MIN"]:
+                    self.epsilon = max(self.hyperparameters["EPSILON_MIN"], self.epsilon * self.hyperparameters["EPSILON_DECAY"])
+                self.counter += 1
 
-    def load(self):
-        raise NotImplementedError
+
+
+    def build_network(self, n_features, n_actions):
+        model = Sequential([
+            Dense(32, input_shape=n_features),
+            Activation("relu"),
+            Dense(64),
+            Activation("relu"),
+            Dense(n_actions),
+            Activation("linear")
+        ])
+        model.compile(loss='mean_squared_error', optimizer=optimizers.Adam(lr=self.hyperparameters["LEARNING_RATE"]))
+        return model
+
+
+    def create_batch(self):
+        if len(self.experience_buffer) >= self.hyperparameters["EXP_BUFFER_SIZE"]:
+            experiences = rsample(self.experience_buffer, self.hyperparameters["BATCH_SIZE"])
+            return experiences
+        return None
+
+
+    def fill_experience_buffer(self):
+        for i in range(self.hyperparameters["EXP_BUFFER_SIZE"]):
+            state = self.simulation.reset()
+            while True:
+                action = self.act(state)
+                next_state, reward, done, _ = self.simulation.step(action)
+                self.remember(state, action, reward, next_state, done)
+                state = deepcopy(next_state)
+                if done:
+                    break
+
+
+    def remember(self, tuple):
+        if len(self.experience_buffer) >= self.hyperparameters["EXP_BUFFER_SIZE"]:
+            self.experience_buffer.pop(0)
+        self.experience_buffer.append(tuple)
+
+    def load(self, file):
+        return load_model(file)
 
     def save(self):
-        raise NotImplementedError
+        self.target_model.save('net_savings/net-' + str(datetime.datetime.now().time()) + '-.h5')
+
+
+    def read_in_configuration_file(self):
+        return yaml.load(open('parameters.yml'))
+
+
+if __name__ == "__main__":
+    agent = JohannesAgent()
