e89359f04f9d7b28f9cd66b8721535b6f12bc86a
Chris Schneider <chrisschneider@Chriss-MacBook-Pro-3.local>
2018-07-14 09:12:25 +0200

    Update

--- agent.py
+++ agent.py
@@ -2,11 +2,13 @@
 
 class Agent:
 
-  def load():
+  def revive(self):
     # TODO: Load agent
+    a = 1
 
-  def next_action(env):
+  def next_action(self, env):
     return 'MUSIC_A'
 
-  def reward(env, action, reward):
+  def rewardLastAction(self, reward):
     # TODO: Reward agent
+    a = 1
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 from flask import Flask, request, abort, jsonify
-from agent import agent
+from agent import Agent
 
 app = Flask(__name__)
-agent.load()
-choice_to_reward = None
+agent = Agent()
+
+agent.revive()
 
 @app.route('/')
 def hello():
@@ -12,22 +13,21 @@ def hello():
 
 @app.route('/next_action')
 def next_action():
-  if params.get('gender') is None: abort(400, "gender missing")
-  if params.get('age') is None: abort(400, "age missing")
-  if params.get('music_on') is None: abort(400, "music_on missing")
-  if params.get('has_sunglasses') is None: abort(400, "has_sunglasses missing")
-  if params.get('step') is None: abort(400, "step missing")
-  if params.get('happiness') is None: abort(400, "happiness missing")
+  if request.args.get('gender') is None: abort(400, "gender missing")
+  if request.args.get('age') is None: abort(400, "age missing")
+  if request.args.get('music_on') is None: abort(400, "music_on missing")
+  if request.args.get('has_sunglasses') is None: abort(400, "has_sunglasses missing")
+  if request.args.get('step') is None: abort(400, "step missing")
+  if request.args.get('happiness') is None: abort(400, "happiness missing")
   env = {
-    'gender': params.get('gender', type=int),
-    'age': params.get('gender', type=int),
-    'music_on': params.get('gender', type=int),
-    'has_sunglasses': params.get('has_sunglasses', type=int),
-    'step': params.get('step', type=int),
-    'happiness': params.get('happiness', type=float)
+    'gender': request.args.get('gender', type=int),
+    'age': request.args.get('gender', type=int),
+    'music_on': request.args.get('gender', type=int),
+    'has_sunglasses': request.args.get('has_sunglasses', type=int),
+    'step': request.args.get('step', type=int),
+    'happiness': request.args.get('happiness', type=float)
   }
   action = agent.next_action(env)
-  choice_to_reward = (env, action)
   return jsonify({
     'action': action,
     'env': env
@@ -35,11 +35,9 @@ def next_action():
 
 @app.route('/observe')
 def observe():
-  if params.get('happiness') is None: abort(400, "happiness missing")
-  happiness = params.get('happiness', type=float)
-  if choice_to_reward is not None:
-    agent.reward(choice_to_reward[0], choice_to_reward[1], happiness)
-    choice_to_reward = None
+  if request.args.get('happiness') is None: abort(400, "happiness missing")
+  happiness = request.args.get('happiness', type=float)
+  agent.rewardLastAction(happiness)
   return "OK"
 
 if __name__ == "__main__":
